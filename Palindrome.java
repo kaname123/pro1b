6bd04b79dec4453518673cc3fca6972bd987e027
kaname123 <cc.lauhoi@gmail.com>
2017-04-25 01:59:43 +0800

    Update Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -6,44 +6,49 @@ public class Palindrome {
 
 
 
-    public static void  main(String args[]){
-
-    }
+   
 
     public static Deque<Character> wordToDeque(String word){
-        ArrayDequeSolution<Character> a = new ArrayDequeSolution<>();
+        Deque<Character> a = new ArrayDequeSolution<>();
         for(int i=0;i<word.length();i++){
             a.addLast(word.charAt(i));
         }
 
         return a;
     }
-    public static boolean isPalindrome(String word){
+   /* public static boolean isPalindrome(String word){
 
-        //public static boolean p(String a){
-	if(word.length()==1||word.isEmpty())
+        
+	if(word.length()<=1)
 		return true;
-	//if(a.length()==2&&(a.charAt(0)==a.charAt(1)))
-		//return true;
+	
 	if(word.charAt(0)==word.charAt(word.length()-1)){
 		String b=word.substring(1, word.length()-1);
 		return isPalindrome(b);
 	}
 	else
 		return false;
-}
+}*/
+	public static boolean isPalindrome(String word){
+	return isPalindromeHelper(wordToDeque(word))
+	}
+	public static boolean isPalindromeHelper(Deque<Character> word){ 
 
-       /* Deque<Character> b=wordToDeque(word);
-        if(b.size()==1||b.isEmpty())
+        if(word.size()<=1)
             return true;
-        if(b.get(0)==b.get(b.size()-1)){
-            b.removeFirst();
-            b.removeLast();
-            String c=b.toString();
-            return isPalindrome(c);
-        }
-        else
-            return false;}*/
+        //if(b.get(0)==b.get(b.size()-1)){
+          //  b.removeFirst();
+           // b.removeLast();
+            //String c=b.toString();
+           // return isPalindrome(c);
+       // }
+       // else
+          //  return false;
+		if(word.removeLast()==word.removeFirst()){
+		return isPalindromeHelper(word);
+		}
+		return false;}
+			
 
 
     public static boolean isPalindrome(String word, CharacterComparator cc){
